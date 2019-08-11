6e5826e58842661a9cb243faaac7b3ee8f802c62
mohemohe <mohemohe@users.noreply.github.com>
2019-08-12 02:12:07 +0900

    db:migrateもmanagerノードからできるようにした

--- manage/db_migrate.sh
+++ manage/db_migrate.sh
@@ -1,6 +1,12 @@
-#!/bin/bash -xe
+#!/bin/bash
 
-cd ../swarm/mastodon
-docker-compose -f docker-compose.manage.yml pull
-docker-compose -f docker-compose.manage.yml run --rm web rails db:migrate
-docker-compose -f docker-compose.manage.yml down --rmi all
+set -x
+
+for NODE in $(docker node ls -q); do
+  SSH_ADDR="$(docker node inspect "${NODE}" | jq -r '.[0].Status.Addr')"
+  CONTAINER_ID="$(ssh "${SSH_ADDR}" docker ps -f 'name=mastodon_web' -q | head -1)"
+  if [ "${CONTAINER_ID}" != "" ]; then
+    ssh "${SSH_ADDR}" docker exec -i "${CONTAINER_ID}" /usr/bin/env rails db:migrate
+    exit $?
+  fi
+done
